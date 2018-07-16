85d7d2f0c432ff37827390f03f3af464a3ea4f82
Benedikt Schmotzle - byte_swap <code@schmotzle.info>
2018-07-16 11:26:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ cd $(dirname $0)
 ln -f -s $(pwd)/vimrc ~/.vimrc
 ln -f -s $(pwd)/tmux.conf ~/.tmux.conf
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
